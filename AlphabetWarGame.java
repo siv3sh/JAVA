b8f0a32557ce07c7e8c92b746a65f44fcb4fbf75
Sivesh <135200528+siv3sh@users.noreply.github.com>
2024-10-29 12:34:18 +0530

    AlphabetWarGame.java

--- AlphabetWarGame.java
+++ AlphabetWarGame.java
@@ -1,12 +1,29 @@
 import java.util.Scanner;
 
-public class Alpha {
-    // Strength points for each letter
-    private final int w = 4, p = 3, b = 2, s = 1;
-    private final int m = 4, q = 3, d = 2, z = 1;
+public class AlphabetWarGame {
+    private int w, p, b, s;
+    private int m, q, d, z;
+
+    public AlphabetWarGame() {
+        this.w = 4;
+        this.p = 3;
+        this.b = 2;
+        this.s = 1;
+        this.m = 4;
+        this.q = 3;
+        this.d = 2;
+        this.z = 1;
+    }
 
     public void playGame(String input) {
-        int leftStrength = 0, rightStrength = 0;
+        int leftStrength = 0;
+        int rightStrength = 0;
+
+        // Validate input to contain only valid characters
+        if (!input.matches("[mqdzwpbs]+")) {
+            System.out.println("Invalid input. Please enter only the characters m, q, d, z, w, p, b, s.");
+            return;
+        }
 
         for (char c : input.toCharArray()) {
             switch (c) {
@@ -21,10 +38,48 @@ public class Alpha {
             }
         }
 
-        // Print result based on strength
         if (leftStrength == 0 && rightStrength == 0) {
             System.out.println("Let's play again! No valid letters found.");
-        } else if (leftStrength > rightStrength) {
+            return;
+        }
+
+        determineWinner(leftStrength, rightStrength);
+    }
+
+    public void playGame(String leftSide, String rightSide) {
+        int leftStrength = calculateLeftStrength(leftSide);
+        int rightStrength = calculateRightStrength(rightSide);
+        determineWinner(leftStrength, rightStrength);
+    }
+
+    private int calculateLeftStrength(String leftSide) {
+        int strength = 0;
+        for (char c : leftSide.toCharArray()) {
+            switch (c) {
+                case 'm': strength += m; break;
+                case 'q': strength += q; break;
+                case 'd': strength += d; break;
+                case 'z': strength += z; break;
+            }
+        }
+        return strength;
+    }
+
+    private int calculateRightStrength(String rightSide) {
+        int strength = 0;
+        for (char c : rightSide.toCharArray()) {
+            switch (c) {
+                case 'w': strength += w; break;
+                case 'p': strength += p; break;
+                case 'b': strength += b; break;
+                case 's': strength += s; break;
+            }
+        }
+        return strength;
+    }
+
+    private void determineWinner(int leftStrength, int rightStrength) {
+        if (leftStrength > rightStrength) {
             System.out.println("Left side wins");
         } else if (rightStrength > leftStrength) {
             System.out.println("Right side wins");
@@ -37,7 +92,7 @@ public class Alpha {
         Scanner scanner = new Scanner(System.in);
         AlphabetWarGame game = new AlphabetWarGame();
 
-        // Loop for continuous gameplay
+        // Keep playing until valid input
         while (true) {
             System.out.println("Enter a string for Alphabet War Game:");
             String input = scanner.nextLine().toLowerCase();
